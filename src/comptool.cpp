6f19d2407b0035dc5d036ab477419248eccf3302
Shingo Tomioka <s.tomioka.103@gmail.com>
2016-09-21 23:05:40 +0900

    Clean comtool.cpp

--- src/comptool.cpp
+++ src/comptool.cpp
@@ -6,136 +6,152 @@ int main(int argc, char** argv){
     return 0;
 }
 
+
 string basename(const string& path){
-  return path.substr(path.find_last_of('/') + 1);
+    return path.substr(path.find_last_of('/') + 1);
 }
 
-// コマンドに応じて動作を呼び分ける
+
 void CompTool::run_command(int argc, char** argv){
-	argc--; argv++;
-	string command = argv[0];
-	if(command == "align"){
+    argc--; argv++;
+    const string command = argv[0];
+    if(command == "align") {
         int* SA = create_SA(argv[1]);
         search_alignment(argc, argv, SA);
         delete SA;
-    }else if(command == "chain"){
+    }
+    else if(command == "chain") {
         chain_alignment(argc, argv);
-	}else if(command == "all"){
-		int* SA = create_SA(argv[1]);
+    }
+    else if(command == "all") {
+        int* SA = create_SA(argv[1]);
         search_alignment(argc, argv, SA);
         delete SA;
         chain_alignment(argc, argv);
-	}else
-		cout << " Invalid command > \"" << command << "\"" << endl;
+    }
+    else {
+        cout << " Invalid command > \"" << command << "\"" << endl;
+    }
 }
 
-// 配列1をファイルから読み込んでsuffix-arrayを構築
+
 int* CompTool::create_SA(const char* file){
-    // seq1をファイルから読み込んで、seq1_size_も取得
-    // Induced Sorting はint型配列で行う
+    // Perform induced sorting with int array
     int* seq1_int = read_fasta_and_create_int_array(file, seq1_size_);
     int* SA = new int[seq1_size_];
     IS is(seq1_int, SA, seq1_size_, num_char_);
     is.run();
-	// seq1をint8_t型の配列にコピー
-	seq1_ = new int8_t[seq1_size_];
-	copy_int_array_to_int8_t_array(seq1_int, seq1_, seq1_size_);
+
+    seq1_ = new int8_t[seq1_size_];
+    copy_int_array_to_int8_t_array(seq1_int, seq1_, seq1_size_);
     delete seq1_int;
     return SA;
 }
 
-// 配列2からk-merをqueryとして取り出して、配列1のBWTを使ってマッチする位置を検索
+
 // argv[1]: seq1, argv[2]: seq2
 // (OPTION)
 // [-k k-mer] : k-mer
 // [-l slide_letters] : k-mer の query を取るとき何文字ずつずらすか
-// [-m max_num_matches] : query に対するマッチの数が何個のものまで出力するか 
+// [-m max_num_matches] : query に対するマッチの数が何個のものまで出力するか
 // [-i] : bwt_interval
 // [-s] : outputs start position
 // [-f] : searches forward only
 // [-b] : searches backward only
 void CompTool::search_alignment(int argc, char** argv, int* SA){
     if(argc < 3) {cout << "File not enough" << endl; exit(1);}
-    // OPTION
+
+    const string seq1_file = argv[1];
+    const string seq2_file = argv[2];
+
+    // Set default values for options
     int k = 15;
     int bwt_interval = 1;
     int slide_letters = 1;
-    int max_num_matches = 100000;  // tandem repeat を検出するとき
-    // int max_num_matches = 5;      // unique な領域を検出するとき
+    int max_num_matches = 1000000000;
     bool search_forward = true;
     bool search_backward = true;
-    bool option_s_valid = false;  // デフォルトではアラインメントの形式で出力
+    bool outputs_start_pos = false;
+
     if(argc > 3){
         for(int i = 3; i < argc; i++){
-            if(argv[i][1] == 'k')  k = atoi(argv[++i]);
-            else if(argv[i][1] == 'l')  slide_letters = atoi(argv[++i]);
-            else if(argv[i][1] == 'm')  max_num_matches = atoi(argv[++i]);
-            else if(argv[i][1] == 'i')  bwt_interval = atoi(argv[++i]);
-            else if(argv[i][1] == 'f')  search_backward = false;
-            else if(argv[i][1] == 'b')  search_forward = false;
-            else if(argv[i][1] == 's')  option_s_valid = true;
+            if(argv[i][1] == 'k')       k                   = atoi(argv[++i]);
+            else if(argv[i][1] == 'l')  slide_letters       = atoi(argv[++i]);
+            else if(argv[i][1] == 'm')  max_num_matches     = atoi(argv[++i]);
+            else if(argv[i][1] == 'i')  bwt_interval        = atoi(argv[++i]);
+            else if(argv[i][1] == 'f')  search_backward     = false;
+            else if(argv[i][1] == 'b')  search_forward      = false;
+            else if(argv[i][1] == 's')  outputs_start_pos   = true;
         }
     }
-    // seq1のBWTを用意
+    // Create BWT from seq1
     BWT bwt(seq1_, SA, seq1_size_, num_char_, bwt_interval);
-    // seq2の読み込み
-    seq2_ = read_fasta_and_create_int8_t_array(argv[2], seq2_size_);
-    // 順向きでqueryにマッチする位置を検索し出力
+
+    seq2_ = read_fasta_and_create_int8_t_array(seq2_file, seq2_size_);
+
+    // Search forward matches
     if(search_forward){
-        stringstream ofs_name;
-	string seq1_name = basename(argv[1]);
-	string seq2_name = basename(argv[2]);
-        if(option_s_valid)
-             ofs_name << "alignments-forward-startpos_" << seq1_name << "_" << seq2_name
-                      << ".tsv";
-        else ofs_name << "alignments-forward-for-chaining_" << seq1_name << "_" << seq2_name
-                      << ".tsv";
-        ofstream ofs(ofs_name.str().c_str());
-        ofs << "#" << argv[2] << "\t" << argv[1] << endl; // ヘッダー
-        for(int i = 0; i < seq2_size_-k; i += slide_letters){
+        const string seq1_name = basename(seq1_file);
+        const string seq2_name = basename(seq2_file);
+        stringstream out_file;
+        if(outputs_start_pos)
+            out_file << "alignments-forward-startpos_" << seq1_name << "_" << seq2_name << ".tsv";
+        else
+            out_file << "alignments-forward-for-chaining_" << seq1_name << "_" << seq2_name << ".tsv";
+        ofstream ofs(out_file.str().c_str());
+
+        ofs << "#" << seq2_file << "\t" << seq1_file << endl;  // header
+        for(int i = 0; i < seq2_size_ - k; i += slide_letters){
             int8_t* query = &seq2_[i];
-            int lb, ub;     // SA上でマッチした位置の始めと終わり
+            int lb, ub;  // lower-bound and upper-bound of matches in suffix array
             bwt.search(query, k, lb, ub);
             if(lb <= ub){
                 for(int j = lb; j <= ub; j++){
                     if(j == lb + max_num_matches) break;
-                    if(option_s_valid) output_startpos(ofs, i, SA[j]);
-                    else output_alignment_forward(ofs, i, SA[j], k);
+                    if(outputs_start_pos)
+                        output_startpos(ofs, i, SA[j]);
+                    else
+                        output_alignment_forward(ofs, i, SA[j], k);
                 }
             }
         }
     }
-    // queryを相補鎖に変換して逆向きにマッチする位置を検索し出力
+
+    // Search reverse complement matches
     if(search_backward){
-        stringstream ofs_name;         
-	string seq1_name = basename(argv[1]);
-	string seq2_name = basename(argv[2]);
-        if(option_s_valid)
-             ofs_name << "alignments-backward-startpos_" << seq1_name << "_" << seq2_name
-                      << ".tsv";
-        else ofs_name << "alignments-backward-for-chaining_" << seq1_name << "_" << seq2_name
-                      << ".tsv";
-        ofstream ofs(ofs_name.str().c_str());
-        ofs << "#" << argv[2] << "\t" << argv[1] << endl; // ヘッダー
+        const string seq1_name = basename(seq1_file);
+        const string seq2_name = basename(seq2_file);
+        stringstream out_file;
+        if(outputs_start_pos)
+            out_file << "alignments-backward-startpos_" << seq1_name << "_" << seq2_name << ".tsv";
+        else
+            out_file << "alignments-backward-for-chaining_" << seq1_name << "_" << seq2_name << ".tsv";
+        ofstream ofs(out_file.str().c_str());
+
+        ofs << "#" << seq2_file << "\t" << seq1_file << endl;  // header
         int8_t* query = new int8_t[k];
-        for(int i = k-1; i < seq2_size_-1; i += slide_letters){
+        for(int i = k-1; i < seq2_size_ - 1; i += slide_letters){
+            // convert k-mers to reverse complements
             for(int j = 0; j < k; j++)
-                query[j] = num_char_- seq2_[i-j];  // 相補鎖に変換して逆向きに入れる
-            int lb, ub;    // SA上でマッチした位置の始めと終わり
+                query[j] = num_char_ - seq2_[i-j];
+
+            int lb, ub;  // lower-bound and upper-bound of matches in suffix array
             bwt.search(query, k, lb, ub);
             if(lb <= ub){
                 for(int j = lb; j <= ub; j++){
                     if(j == lb + max_num_matches) break;
-                    if(option_s_valid) output_startpos(ofs, i, SA[j]);
-                    else output_alignment_backward(ofs, i, SA[j], k);
+                    if(outputs_start_pos)
+                        output_startpos(ofs, i, SA[j]);
+                    else
+                        output_alignment_backward(ofs, i, SA[j], k);
                 }
             }
         }
-        delete query;
+        delete[] query;
     }
 }
 
-// アラインメントをChainingする
+
 // argv[1]: seq1, argv[2]: seq2
 // (OPTION) [-n]: near_dist
 // [-f]: runs forward only, [-b]: runs backward only
@@ -151,41 +167,49 @@ void CompTool::chain_alignment(int argc, char** argv){
             else if(argv[i][1] == 'f')  runs_backward = false;
             else if(argv[i][1] == 'b')  runs_forward = false;
     }
-    // 出力先ファイル
-    stringstream ofs_name;
-    ofs_name << "chains_" << argv[1] << "_" << argv[2] << ".tsv";
-    ofstream ofs(ofs_name.str().c_str());
-    ofs << "#" << argv[2] << "\t" << argv[1] << endl;
-    
-    // alignments_forwardをファイルから読み込んでChaininig
+
+    const string seq1_file = argv[1];
+    const string seq2_file = argv[2];
+
+    const string seq1_name = basename(seq1_file);
+    const string seq2_name = basename(seq2_file);
+
+    stringstream out_file;
+    out_file << "chains_" << seq1_name << "_" << seq2_name << ".tsv";
+    ofstream ofs(out_file.str().c_str());
+    ofs << "#" << seq2_name << "\t" << seq1_name << endl;
+
     if(runs_forward){
         stringstream ifs1_name;
-        ifs1_name << "alignments-forward-for-chaining_" << argv[1] << "_" << argv[2]
+        ifs1_name << "alignments-forward-for-chaining_" << seq1_name << "_" << seq2_name
                   << ".tsv";
         run_chaining(ifs1_name.str(), ofs, near_dist);
     }
-    // alignments_backwardをファイルから読み込んでChaininig
+
     if(runs_backward){
         stringstream ifs2_name;
-        ifs2_name << "alignments-backward-for-chaining_" << argv[1] << "_" << argv[2]
+        ifs2_name << "alignments-backward-for-chaining_" << seq1_name << "_" << seq2_name
                   << ".tsv";
         run_chaining(ifs2_name.str(), ofs, near_dist);
     }
 }
 
-// アラインメントの入ったファイルを受け取ってChainingを行い、結果を出力
+
+// Receive an alignment file, run chaining and output a chain file
 void CompTool::run_chaining(string file, ofstream& ofs, const int near_dist){
+    // Count alignments
     ifstream ifs;
     ifs.open(file.c_str());
     if(!ifs.is_open()) {cout << "File not found\n> " << file << endl; exit(1);}
     string line;
     int num_alignment = 0;
-    getline(ifs,line);  // ヘッダーの読み飛ばし
-    while(getline(ifs, line)) num_alignment++;   // アラインメントの数を数える
+    getline(ifs, line);  // Skip header
+    while(getline(ifs, line)) num_alignment++;
     ifs.close();
+
+    // Load alignments
     ifs.open(file.c_str());
-    getline(ifs,line);  // ヘッダーの読み飛ばし
-	// アラインメントを読み込んで格納
+    getline(ifs, line);  // Skip header
     Alignment* alignments = new Alignment[num_alignment];
     int* buf = new int[5];
     for(int i = 0; i < num_alignment; i++){
@@ -194,8 +218,9 @@ void CompTool::run_chaining(string file, ofstream& ofs, const int near_dist){
         }
         alignments[i].set(buf);
     }
-    delete buf;
-	// Chaining
+    delete[] buf;
+
+    // Run chaining
     Chaining chaining(alignments, num_alignment, near_dist);
     chaining.run();
     chaining.output_major_chains(ofs);
@@ -224,8 +249,9 @@ int* CompTool::read_fasta_and_create_int_array(const char* file, int& size){
     return s;
 }
 
+
 // fastaファイルを読み込んで必要なメモリを確保し、'ACGTN'を'12340'に変換して格納する (int8_t版)
-int8_t* CompTool::read_fasta_and_create_int8_t_array(const char* file, int& size){
+int8_t* CompTool::read_fasta_and_create_int8_t_array(const string& file, int& size){
     ifstream ifs;
     ifs.open(file);
     if(!ifs.is_open()) {cout << "File not found\n>" << file << endl; exit(1);}
@@ -246,9 +272,9 @@ int8_t* CompTool::read_fasta_and_create_int8_t_array(const char* file, int& size
     return s;
 }
 
+
 // intの配列の中身をint8_tの配列にコピーする
-void CompTool::copy_int_array_to_int8_t_array(int* int_array, int8_t* int8_array, const int size){  
+void CompTool::copy_int_array_to_int8_t_array(int* int_array, int8_t* int8_array, const int size){
     for(int i = 0 ; i < size; i++)
         int8_array[i] = int_array[i];
 }
-
